1c69718bc1af307b03b177f1d8d05099cc35f195
André Eriksson <eandre@gmail.com>
2016-04-09 19:43:37 +0200

    after

--- a.go
+++ a.go
@@ -8,13 +8,5 @@ func Open(name string) (*Reader, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Reader{f: f, r: bufio.NewReader(f)}, nil
-}
-
-func (w *Writer) Write(p []byte) (int, error) {
-	return w.w.Write(p)
-}
-
-func (w *Writer) WriteString(p string) (int, error) {
-	return w.w.WriteString(p)
+	return &Reader{f: f, Reader: bufio.NewReader(f)}, nil
 }
